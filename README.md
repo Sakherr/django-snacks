05cac7e90532eb40296c9d30e28b1ac6b66a9387
Sakherr <sakhershteyat@gmail.com>
2023-07-10 17:12:51 +0000

    sec commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# LAB - Class 26
+
+# Project: django-snacks
+
+# Author: Sakher shteyat
+
+
+
+# Setup
+
+ PORT - 80000
+
+ # How to initialize/run your application (where applicable)
+  python manage.py runserver
+
+
+# Tests
+
+ python manage.py test 
---
+++ snacks/tests.py
@@ -0,0 +1,27 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+class TestHome(SimpleTestCase):
+    def test_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+   
+
+    def test_home_page_templates(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+
+class Testabout(SimpleTestCase):
+ 
+    def test_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+    
+    def test_about_page_templates(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
